4d4f475139ac2c5ad6569cf6b532f0f9f555934c
patricia <patricia@localhost.localdomain>
2022-11-26 17:56:59 +0100

    Test 2

--- tests/FizzBuzzTest.php
+++ tests/FizzBuzzTest.php
@@ -9,6 +9,12 @@ Class FizzBuzzTest extends TestCase{
         $cadena = $fizzbuzz->fizzbuzz(13);
         $this->assertEquals("13", $cadena, "No coincide la cadena");
     }
+
+    public function testConvertNumberString2(){
+        $fizzbuzz = new App\FizzBuzz();
+        $cadena = $fizzbuzz->fizzbuzz(7);
+        $this->assertEquals("7", $cadena, "No coincide la cadena");
+    }
 }
 
 ?>
